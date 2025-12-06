57f15c5a8feb565467231740d52ec63682e4ee94
adjoannek <joannekwarteng@gmail.com>
2025-12-05 20:33:21 -0500

    added tabs to index.html

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello, World!");
+<!-- JS for Switching Tabs -->
+<script>
+    const buttons = document.querySelectorAll('.tab-button');
+    const sections = document.querySelectorAll('.content-section');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const tab = button.dataset.tab;
+
+            
+            buttons.forEach(btn => btn.classList.remove('active'));
+            // remove active from content
+            sections.forEach(sec => sec.classList.remove('active'));
+
+            // activate new ones
+            button.classList.add('active');
+            document.getElementById(tab).classList.add('active');
+        });
+    });
+</script>
